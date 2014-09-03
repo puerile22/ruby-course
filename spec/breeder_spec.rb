ec6068a628756d3e0a219a60acc5ac53a4bd947f
Peng Gao <puerile22@gmail.com>
2014-09-02 17:56:41 -0700

    second spec

--- spec/breeder_spec.rb
+++ spec/breeder_spec.rb
@@ -1,48 +1,67 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Breeder do
+	let (:breeder) {PuppyBreeder::Breeder.new("Jon")}
+	let(:customer) {PuppyBreeder::Customer.new("Peng")}
+	let(:puppy_list) {PuppyBreeder::Puppy_list.new}
 	describe "#initialize" do
 		it "create a breeder instance" do
-			breeder=PuppyBreeder::Breeder.new("Jon")
 			expect(breeder.name).to eq('Jon')
 		end
 	end
 	describe "#add_puppy" do
 		it "allows breeder to add new puppies to sell" do
 			breeder=PuppyBreeder::Breeder.new("Jon")
-			breeder.add_puppy("Leo",2)
-			expect(breeder.puppy_list).to eq({"Leo"=>2})
+			breeder.add_puppy("Leo",'Alaskan',200)
+			expect(breeder.puppy_list).to eq({"Leo"=>['Alaskan',200,700]})
 		end
 	end
 	describe "#input_request" do
 		it "allows breeder to manually input a purchase request" do
-			breeder=PuppyBreeder::Breeder.new("Jon")
-			breeder.add_puppy('Leo',2)
+			breeder.add_puppy('Leo','Alaskan',200)
 			breeder.input_request
-			expect(breeder.purchase_request).to eq({"Peng"=>["Leo",2]})
+			expect(breeder.purchase_request).to eq({"Peng"=>['Leo','Alaskan',200,700]})
 		end
 		it "will return nil if the breeder doesn't have this puppy" do
-			breeder=PuppyBreeder::Breeder.new("Jon")
 			expect(breeder.input_request).to be_nil
 		end
 	end
 	describe "#accept_request" do
 		it "allows breeder to accept purchase request" do
-			breeder=PuppyBreeder::Breeder.new("Jon")
-			breeder.add_puppy('Leo',2)
-			customer=PuppyBreeder::Customer.new("Peng")
-			puppy_list=PuppyBreeder::Puppy_list.new
+			breeder.add_puppy('Leo','Alaskan',200)
 			puppy=breeder.puppy
-			puppy_list.add(puppy.name,puppy.age)
-			customer.make_request(breeder,puppy,400)
+			puppy_list.add(puppy)
+			customer.make_request('Alaskan')
 			breeder.purchase_list.add(customer.request)
-			expect(puppy_list.list).to eq({'Leo'=>2})
+			expect(puppy_list.list).to eq({'Leo'=>['Alaskan',200,700]})
 			breeder.generate_request
-			expect(breeder.purchase_request).to eq({"Peng"=>["Leo",2]})
-			breeder.accept_requests(customer)
+			expect(breeder.purchase_request).to eq({"Peng"=>['Leo','Alaskan',200,700]})
+			breeder.accept_requests(customer.name)
 			expect(breeder.purchase_request).to eq({})
-			expect(breeder.completed_request).to eq({"Peng"=>["Leo",2]})
+			expect(breeder.completed_request).to eq({"Peng"=>['Leo','Alaskan',200,700]})
 			expect(breeder.puppy_list).to eq({})
+		end
+	end
+	describe "#generate_request" do
+		it "put a purchase request on hold if there's no puppy suitable for the customer, and the breeder won't see the requests on hold. 
+If a new puppy is available, the customer who made the request first gets the puppy first" do
+			breeder.add_puppy('Leo','Alaskan',200)
+			puppy=breeder.puppy
+			puppy_list.add(puppy)
+			customer.make_request('Hound')
+			breeder.purchase_list.add(customer.request)
+			expect(puppy_list.list).to eq({'Leo'=>['Alaskan',200,700]})
+			breeder.generate_request
+			expect(breeder.purchase_request).to eq({})
+			expect(breeder.on_hold_request).to eq([{'Peng'=>'Hound'}])
+			breeder.add_puppy('Yuki','Hound',300)
+			breeder.generate_request
+			expect(breeder.purchase_request).to eq({'Peng'=>['Yuki','Hound',300,1000]})
+			expect(breeder.on_hold_request).to eq([])
+			breeder.accept_requests(customer.name)
+			expect(breeder.purchase_request).to eq({})
+			expect(breeder.completed_request).to eq({"Peng"=>['Yuki','Hound',300,1000]})
+			expect(breeder.puppy_list).to eq({'Leo'=>['Alaskan',200,700]})
 		end
 	end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,21 +1,21 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Customer do
-	describe "#initialize" do 
+	let(:customer) {PuppyBreeder::Customer.new("Peng")}
+	let(:breeder) {PuppyBreeder::Breeder.new('Jon')}
+	let(:list) {PuppyBreeder::Purchase_list.new}
+	describe "#initialize" do
 		it "create a customer instance" do
-			customer=PuppyBreeder::Customer.new("Peng")
 			expect(customer.name).to eq('Peng')
 		end
 	end
 	describe "#make_request" do
 		it "allows the customer to make a purchase request" do
-			customer=PuppyBreeder::Customer.new("Peng")
-			breeder=PuppyBreeder::Breeder.new('Jon')
-			puppy=PuppyBreeder::Puppy.new('Leo',3)
-			list=PuppyBreeder::Purchase_list.new
-			customer.make_request(breeder,puppy,400)
+			breeder.add_puppy('Leo','Alaskan',200)
+			breed=breeder.puppy_list['Leo'][0]
+			customer.make_request(breed)
 			list.add(customer.request)
-			expect(list.list).to eq({"Peng"=>['Jon','Leo',400]})
+			expect(list.list).to eq({"Peng"=>"Alaskan"})
 		end
 	end
 
